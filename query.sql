81dd639a63fd665299f4dc50d265a86abf5338d2
Muhammad Zosa Romero <zoromero00@gmail.com>
2026-02-08 20:49:18 +0700

    Add SQL queries for customer and order analysis

---
+++ query.sql
@@ -0,0 +1,15 @@
+-- 1. Tampilkan nama customer, kota, judul buku, harga, & tanggal order hanya untuk customer dari Bandung atau Jakarta dan harga buku ≥ 180.000. Urutkan dari harga tertinggi ke terendah
+SELECT 
+
+-- 2. Cari total belanja per kota, lalu tampilkan hanya kota dengan total belanja > 3.000.000.
+
+
+/* 3. Tampilkan daftar pesanan yang memenuhi syarat:
+- Tanggal order antara 1 Februari 2024 – 31 Maret 2024
+- Judul buku mengandung kata “SQL” atau “PostgreSQL”
+- Tampilkan nama customer, kota, judul buku, harga, dan tanggal order
+- Urutkan berdasarkan tanggal terbaru → terlama. */
+
+
+-- 4. Tampilkan nama customer, kota, jumlah buku yang dibeli, total uang yang dihabiskan, & rata-rata harga buku yang dibeli. Hanya tampilkan customer yang membeli minimal 4 buku. Urutkan dari total belanja tertinggi.
+  
