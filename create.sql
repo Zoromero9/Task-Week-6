2234d923b69d5111ee168dc8620546dcc277f570
Muhammad Zosa Romero <zoromero00@gmail.com>
2026-02-08 22:22:14 +0700

    Create customers and orders tables in SQL

--- create.sql
+++ create.sql
@@ -1,1 +1,13 @@
+CREATE TABLE customers (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  city TEXT NOT NULL
+);
 
+CREATE TABLE orders (
+  id SERIAL PRIMARY KEY,
+  course TEXT NOT NULL,
+  price INT NOT NULL,
+  customer_id INT REFERENCES customers(id),
+  order_date DATE
+);
